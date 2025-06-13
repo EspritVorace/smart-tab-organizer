343a819f4b05b11222f2c560059ab6f4aec79ddd
EspritVorace <101243416+EspritVorace@users.noreply.github.com>
2025-06-13 18:15:27 +0200

    Fix Storybook design system initialization

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,14 @@
 // Atlaskit design system addon
-import designSystem from '@atlaskit/storybook-addon-design-system';
-
-designSystem();
+import designSystem from '@atlaskit/storybook-addon-design-system/preview';
 
 // Provide a dummy chrome.i18n implementation for getMessage
 if (!globalThis.chrome) {
   globalThis.chrome = { i18n: { getMessage: (key) => key } };
 }
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+export default {
+  ...designSystem,
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' }
+  }
 };
