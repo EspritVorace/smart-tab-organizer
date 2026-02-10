71c882011d13c74b9e288d12a4b3d4ce8ccd1f87
EspritVorace <seb0678+github@gmail.com>
2026-02-10 10:10:24 +0100

    Bump version to 1.0.3 in wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   manifest: {
     name: '__MSG_extensionName__',
     description: '__MSG_extensionDescription__',
-    version: '1.0.1',
+    version: '1.0.3',
     author: 'EspritVorace',
     homepage_url: 'https://github.com/EspritVorace/smart-tab-organizer',
     default_locale: 'en',
